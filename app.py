8e74862c5ed1b5148300e8b404cf034f4ac7d7dc
BeatrizLandi <beatriz.landi.coelho@gmail.com>
2023-05-11 20:09:08 -0300

    v1

---
+++ app.py
@@ -0,0 +1,35 @@
+
+#___________________________________IMPORTS_________________________
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+
+
+
+def send_email(subject, body, to):
+    # Set up the email parameters
+    msg = MIMEMultipart()
+    msg['From'] = "andressa.fideliscoelho@gmail.com"
+    msg['To'] = to
+    msg['Subject'] = subject
+    msg.attach(MIMEText(body, 'plain'))
+
+    # Log in to the Gmail SMTP server
+    smtp_server = smtplib.SMTP('smtp.gmail.com', 587)
+    smtp_server.ehlo()
+    smtp_server.starttls()
+    smtp_server.login("andressa.fideliscoelho@gmail.com", "ihgxlnrazynkkcxq")
+
+    # Send the email
+    text = msg.as_string()
+    smtp_server.sendmail("andressa.fideliscoelho@gmail.com", to, text)
+
+    # Close the SMTP server
+    smtp_server.quit()
+    print("Email sent successfully.")
+
+# Example usage
+subject = "Test Email"
+body = "This is a test email sent using Python."
+to = "beatriz.landi.coelho@gmail.com"
+send_email(subject, body, to)
